83a1366bb6ab386e952c26286051a3941748a189
hind23 <lh_dehili@esi.dz>
2023-12-27 16:47:43 +0100

    Home page completed

--- src/components/KeyWord.jsx
+++ src/components/KeyWord.jsx
@@ -3,8 +3,8 @@
 export const KeyWord=(props)=>
 {
     return (
-        <div className="w-fit h-[45px] px-[10px]  bg-gray-300 rounded-[5px] justify-center items-center  inline-flex">
-  <div className="text-gray-900 text-[18px] font-medium font-['TT Commons']">{props.content}</div>
+        <div className="w-fit h-[38px] sm:h-[45px] sm:px-[10px]  bg-gray-300 rounded-[3px] sm:rounded-[5px] justify-center items-center px-2 inline-flex">
+  <div className="text-gray-900 text-[3.5vw] xs:text-[15px] font-medium font-['TT Commons']">{props.content}</div>
 </div>
     )
 }
---
+++ src/components/LittleSideBar.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import myImage from '../assets/favor.svg';
+import setting from '../assets/settings.svg';
+import { LogOut } from './LogOut';
+
+export const LittleSideBar=()=>
+{
+ 
+
+    const handleSave=()=>
+{
+
+}
+const handleChangeSettings=()=>
+{
+
+}
+return( <div className='absolute right-0 px-8'>
+  <div className='bg-sky-950 rounded-[5px] box-content px-4 xs:w-[120px] h-[150px] w-[100px]  ml-auto grid xs:py-4 pb-2 gap-y-3'>
+<div className='flex items-center gap-x-2'>
+   <button onClick={handleSave}>
+<img className=' sm:w-5 w-5 ' src={myImage} />  </button> 
+<h1 className='text-white font-medium'>
+    Favors</h1> 
+</div>
+
+   <hr className='border-1  border-cyan-300'></hr>
+  
+
+
+   <div className='flex items-center gap-x-2'>
+   <button onClick={handleChangeSettings}>
+<img className=' sm:w-5 w-5 ' src={setting} />  </button> 
+<h1 className='text-white font-medium'>
+    Settings</h1> 
+</div>
+
+   <hr className='border-1  border-cyan-300'></hr>
+<LogOut mode='light'/>
+</div>
+
+
+
+</div>
+)
+}
